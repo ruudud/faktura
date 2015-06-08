ab44457ada9b8ef9044e310a0a9891d6b52ce236
Pål Ruud <ruudud@gmail.com>
2015-06-08 17:05:15 +0200

    Update margins and various styling. Add more info at top. Fixes #1.

--- input.js
+++ input.js
@@ -1,5 +1,12 @@
 ;(function() {
-  function bootstrap() {
+  function formatDate(dateInput) {
+    var dateVal = new Date(dateInput);
+    return '' + dateVal.getDate() + '.'
+      + (dateVal.getMonth() + 1) + '.'
+      + dateVal.getFullYear();
+  }
+
+  function addListeners() {
     document
     .getElementById('amount')
     .addEventListener('blur', onChange, false);
@@ -12,18 +19,27 @@
     document
     .getElementById('paymentInfo')
     .addEventListener('blur', onChange, false);
-  
   }
+  function addInvoiceDate() {
+    var sel = 'invoiceDate';
+    var $outs = document.getElementsByClassName(sel);
+    [].forEach.call($outs, function($out) {
+      $out.textContent = formatDate(Date.now());
+    });
+  }
+  function bootstrap() {
+    addListeners();
+    addInvoiceDate();
+  }
+
+
   
   function onChange(ev) {
     var $el = ev.target,
-      newVal, dateVal;
+      newVal;
 
     if ($el.type === 'date') {
-      dateVal = new Date($el.valueAsNumber);
-      newVal = '' + dateVal.getDate() + '/'
-        + (dateVal.getMonth() + 1) + '/'
-        + dateVal.getFullYear();
+      newVal = formatDate($el.valueAsNumber);
     } else {
       newVal =  $el.value.replace(/\n\r?/g, '<br />');
     }
