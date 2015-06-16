98cf7f042b5651abd308821ca47798973d874aa8
Pål Ruud <ruudud@gmail.com>
2015-06-16 14:11:13 +0200

    Templates

--- faktura.js
+++ faktura.js
@@ -1,14 +1,13 @@
 ;(function() {
-  var templates = [
-    {
-      id: 'Krisslee',
+  var TEMPLATES = {
+    'Krisslee': {
       paymentInfo: 'Sponsing',
       accountNumber: '1503.42.91680',
       paidTo: 'Krisslee\nNeseveien 9\n4514 MANDAL',
       orgNumber: '913010779',
       webpage: 'www.krisslee.no'
     }
-  ];
+  };
 
   function formatDate(dateInput) {
     var dateVal = new Date(dateInput);
@@ -27,7 +26,7 @@
       newVal =  $el.value.replace(/\n\r?/g, '<br />');
     }
 
-    var sel = $el.id + 'Output';
+    var sel = 'js-' + $el.id + 'Output';
     var $outs = document.getElementsByClassName(sel);
     [].forEach.call($outs, function($out) {
       $out.innerHTML = newVal;
@@ -42,24 +41,47 @@
     ].forEach(function(id) {
       document
         .getElementById(id)
-        .addEventListener('blur', onChange, false);
+        .addEventListener('change', onChange, false);
     });
-
-    // TODO improve this..
-    document
-      .getElementById('accountNumber')
-      .dispatchEvent(new FocusEvent('blur'));
   }
   function addInvoiceDate() {
-    var sel = 'invoiceDateOutput';
+    var sel = 'js-invoiceDateOutput';
     var $outs = document.getElementsByClassName(sel);
     [].forEach.call($outs, function($out) {
       $out.textContent = formatDate(Date.now());
     });
   }
+
+  function tmplClickHandler(ev) {
+    var $el = ev.currentTarget;
+    var tmplId = $el.dataset.tmplId;
+    var tmpl = TEMPLATES[tmplId];
+    var fields = Object.keys(tmpl);
+    fields.forEach(function(field) {
+      var $inputEl = document.getElementById(field);
+      $inputEl.value = tmpl[field];
+      $inputEl.dispatchEvent(new Event('change'));
+    });
+  }
+
+  function addTemplates() {
+    var $container = document.getElementById('templates');
+    Object.keys(TEMPLATES).forEach(function(tmplId) {
+      var $listEl = document.createElement('li');
+      var $el = document.createElement('a');
+      $el.href = '#';
+      $el.textContent = tmplId;
+      $el.dataset.tmplId = tmplId;
+      $el.addEventListener('click', tmplClickHandler, false);
+      $listEl.appendChild($el);
+      $container.appendChild($listEl);
+    });
+  }
+
   function bootstrap() {
     addListeners();
     addInvoiceDate();
+    addTemplates();
   }
 
   window.addEventListener('load', bootstrap);
